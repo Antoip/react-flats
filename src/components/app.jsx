de17690490a17a0ed094c8bb1614bec18a192ae0
Antoine Piguet <antoine.piguet@gmail.com>
2020-05-28 13:45:17 +0200

    Started app component

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selectedFlat: null
+    };
+  }
+
+  render() {
+    return (<h1>Test</h1>);
+  }
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './app.jsx';
-
 import '../assets/stylesheets/application.scss';
+import App from './components/app';
 
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
