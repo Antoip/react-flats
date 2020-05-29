8684d863c8e5ee3a768f80cba76865e4035f041a
Antoine Piguet <antoine.piguet@gmail.com>
2020-05-29 12:26:39 +0200

    All done

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
 
 import FlatList from './flatlist';
+import Marker from './marker';
 
 class App extends Component {
   constructor(props) {
@@ -16,27 +17,36 @@ class App extends Component {
     this.setState({selectedFlat: this.props.flats[index]});
   }
 
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
+    };
+  }
+
   render() {
     return (
-      <FlatList
-        flats={this.props.flats}
-        selectedFlat={this.state.selectedFlat}
-        selectFlat={this.selectFlat}
-      />,
-      <div className="map-container">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: /* YOUR KEY HERE */ }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
-        >
-          <AnyReactComponent
-              lat={59.955413}
-              lng={30.337844}
-              text="My Marker"
-          />
-        </GoogleMapReact>
+      <div>
+        <FlatList
+          flats={this.props.flats}
+          selectedFlat={this.state.selectedFlat}
+          selectFlat={this.selectFlat}
+        />
+        <div className="map-container">
+          <GoogleMapReact
+            bootstrapURLKeys={{ key: 'AIzaSyCgylbiZKrnByydYJQMMzXnuAJsrlH5-t8' }}
+            defaultCenter={this.center()}
+            defaultZoom={12}
+          >
+            <Marker
+                lat={this.state.selectedFlat.lat}
+                lng={this.state.selectedFlat.lng}
+                text={this.state.selectedFlat.name}
+            />
+          </GoogleMapReact>
+        </div>
       </div>
-      );
+    );
   }
 }
 
---
+++ src/components/marker.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Marker = () => {
+  return <img src="https://img.icons8.com/plasticine/2x/marker.png" alt="" />;
+};
+
+export default Marker;
